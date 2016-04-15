f6e23a438f41f2e05c2bf427b57f3cb9831334f5
zhaoliang <2668645098@qq.com>
2016-04-15 10:13:23 +0800

    change Video player and fix pre layout

--- app/src/main/java/com/zky/articleproj/manager/Player.java
+++ app/src/main/java/com/zky/articleproj/manager/Player.java
@@ -16,135 +16,135 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class Player implements OnBufferingUpdateListener, OnCompletionListener,
-		OnPreparedListener {
+        OnPreparedListener {
 
-	public MediaPlayer mediaPlayer; // 媒体播放器
-	private SeekBar seekBar; // 拖动条
-	private TextView tv_durationView;
-	private Button ib_play;
-	private Timer mTimer = new Timer(); // 计时器
-	public boolean isplay=false;
+    public MediaPlayer mediaPlayer; // 媒体播放器
+    private SeekBar seekBar; // 拖动条
+    private TextView tv_durationView;
+    private Button ib_play;
+    private Timer mTimer = new Timer(); // 计时器
+    public boolean isplay = false;
 
-	// 初始化播放器
-	public Player(SeekBar seekBar,TextView tv_durationView,Button ib_play) {
-		super();
-		this.seekBar = seekBar;
-		this.tv_durationView=tv_durationView;
-		this.ib_play=ib_play;
-		try {
-			mediaPlayer = new MediaPlayer();
-			mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);// 设置媒体流类型
-			mediaPlayer.setOnBufferingUpdateListener(this);
-			mediaPlayer.setOnPreparedListener(this);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		// 每一秒触发一次
-		mTimer.schedule(timerTask, 0, 1000);
-	}
+    // 初始化播放器
+    public Player(SeekBar seekBar, TextView tv_durationView, Button ib_play) {
+        super();
+        this.seekBar = seekBar;
+        this.tv_durationView = tv_durationView;
+        this.ib_play = ib_play;
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);// 设置媒体流类型
+            mediaPlayer.setOnBufferingUpdateListener(this);
+            mediaPlayer.setOnPreparedListener(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 每一秒触发一次
+        mTimer.schedule(timerTask, 0, 1000);
+    }
 
-	public Player() {
-		try {
-			mediaPlayer = new MediaPlayer();
-			mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);// 设置媒体流类型
-			mediaPlayer.setOnBufferingUpdateListener(this);
-			mediaPlayer.setOnPreparedListener(this);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public Player() {
+        try {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);// 设置媒体流类型
+            mediaPlayer.setOnBufferingUpdateListener(this);
+            mediaPlayer.setOnPreparedListener(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	// 计时器
-	TimerTask timerTask = new TimerTask() {
+    // 计时器
+    TimerTask timerTask = new TimerTask() {
 
-		@Override
-		public void run() {
-			if (mediaPlayer == null)
-				return;
-			if (mediaPlayer.isPlaying() && seekBar.isPressed() == false) {
-				handler.sendEmptyMessage(0); // 发送消息
-			}
-		}
-	};
+        @Override
+        public void run() {
+            if (mediaPlayer == null)
+                return;
+            if (mediaPlayer.isPlaying() && seekBar.isPressed() == false) {
+                handler.sendEmptyMessage(0); // 发送消息
+            }
+        }
+    };
 
-	Handler handler = new Handler() {
-		public void handleMessage(android.os.Message msg) {
-			int position = mediaPlayer.getCurrentPosition();
-			int duration = mediaPlayer.getDuration();
-			if (duration > 0) {
-				long pos = seekBar.getMax() * position / duration;
-				String timeStr = TextFormatter.getMusicTime(position+1)+ "/" + TextFormatter.getMusicTime(duration);
-				tv_durationView.setText(timeStr);
-				seekBar.setProgress((int) pos+1);
-			}
-		};
-	};
+    Handler handler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            int position = mediaPlayer.getCurrentPosition();
+            int duration = mediaPlayer.getDuration();
+            if (duration > 0) {
+                long pos = seekBar.getMax() * position / duration;
+                String timeStr = TextFormatter.getMusicTime(position + 1) + "/" + TextFormatter.getMusicTime(duration);
+                tv_durationView.setText(timeStr);
+                seekBar.setProgress((int) pos + 1);
+            }
+        }
 
-	public void play() {
-		mediaPlayer.start();
-		isplay=true;
-		//ib_play.setBackgroundResource(R.drawable.button_pause);
-	}
+        ;
+    };
 
-	/**
-	 *
-	 * @param url
-	 *            url地址
-	 */
-	public void playUrl(String url) {
-		try {
-			mediaPlayer.reset();
-			mediaPlayer.setDataSource(url); // 设置数据源
-			mediaPlayer.prepare(); // prepare自动播放
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (SecurityException e) {
-			e.printStackTrace();
-		} catch (IllegalStateException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public void play() {
+        mediaPlayer.start();
+        isplay = true;
+        //ib_play.setBackgroundResource(R.drawable.button_pause);
+    }
 
-	// 暂停
-	public void pause() {
-		mediaPlayer.pause();
-		isplay=false;
-		//ib_play.setBackgroundResource(R.drawable.button_play);
-	}
+    /**
+     * @param url url地址
+     */
+    public void playUrl(String url) {
+        try {
+            mediaPlayer.reset();
+            mediaPlayer.setDataSource(url); // 设置数据源
+            mediaPlayer.prepare(); // prepare自动播放
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	// 停止
-	public void stop() {
-		if (mediaPlayer != null) {
-			mediaPlayer.stop();
-			mediaPlayer.release();
-			mediaPlayer = null;
-		}
-	}
+    // 暂停
+    public void pause() {
+        mediaPlayer.pause();
+        isplay = false;
+        //ib_play.setBackgroundResource(R.drawable.button_play);
+    }
 
-	@Override
-	public void onPrepared(MediaPlayer mp) {
-		mp.start();
-		Log.e("mediaPlayer", "onPrepared");
-		//ib_play.setBackgroundResource(R.drawable.button_pause);
-	}
+    // 停止
+    public void stop() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
 
-	@Override
-	public void onCompletion(MediaPlayer mp) {
-		Log.e("mediaPlayer", "onCompletion");
-	}
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        mp.start();
+        Log.e("mediaPlayer", "onPrepared");
+        //ib_play.setBackgroundResource(R.drawable.button_pause);
+    }
 
-	/**
-	 * 缓冲更新
-	 */
-	@Override
-	public void onBufferingUpdate(MediaPlayer mp, int percent) {
-		//seekBar.setSecondaryProgress(percent);
-		/*int currentProgress = seekBar.getMax()
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        Log.e("mediaPlayer", "onCompletion");
+    }
+
+    /**
+     * 缓冲更新
+     */
+    @Override
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+        //seekBar.setSecondaryProgress(percent);
+        /*int currentProgress = seekBar.getMax()
 				* mediaPlayer.getCurrentPosition() / mediaPlayer.getDuration();
 		Log.e(currentProgress + "% play", percent + " buffer");*/
-	}
+    }
 
 
 }
--- app/src/main/java/com/zky/articleproj/manager/TextFormatter.java
+++ app/src/main/java/com/zky/articleproj/manager/TextFormatter.java
@@ -5,8 +5,8 @@ import java.util.Date;
 import java.util.Locale;
 
 public class TextFormatter {
-	public static String getMusicTime(long duration) {
-		return new SimpleDateFormat("mm:ss", Locale.CHINA).format(new Date(
-				duration));
-	}
+    public static String getMusicTime(long duration) {
+        return new SimpleDateFormat("mm:ss", Locale.CHINA).format(new Date(
+                duration));
+    }
 }
